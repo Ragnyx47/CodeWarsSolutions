7c631f45180b4bafcac075e845a6e4cb7526d893
Ragnyx47 <jakub3389@gmail.com>
2021-07-05 21:39:34 +0200

    #1 Replace dots solution

---
+++ CodeWarsS/Program.cs
@@ -0,0 +1,14 @@
+﻿using StringUtility;
+using System;
+
+namespace CodeWarsS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Replacer.ReplaceDots("no dots"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringUtility/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StringUtility
+{
+    public static class Replacer
+    {
+        /// <summary>
+        /// Replace all dots
+        /// </summary>
+        /// <param name="str">String with dots</param>
+        /// <returns>Returns string without dots</returns>
+        public static string ReplaceDots(string str)
+        {
+            // Problem: dot (".") is a special character used to match any one character
+            // Solution: a) use ("\.") which means literal dot, 
+            //           b) preceding it with ("escaping it") another ("\")
+            return Regex.Replace(str, "\\.", "-");
+        }
+    }
+}
